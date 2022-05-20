f391766a19e0bbfa758782ec8d128ac5347a8c91
GithubPaltech <msgupta@paltech.ch>
2022-05-20 20:03:22 +0530

    master

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+//<-- All React native imports.... -->
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+
+//<-- All Compoments imports.... -->
+
+//<-- All Utils imports.... -->
 
 export default function App() {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        {/* Search Bar */}
+        <Text>Search</Text>
+      </View>
+      <View style={styles.list}>
+        {/* List area */}
+        <Text>List</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight + 10 : 0,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "skyblue",
+  },
+});
